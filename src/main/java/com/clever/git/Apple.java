0e9c49fba761c6e835d1bfa8338219cd47b56bde
clever <158091461@qq.com>
2019-03-18 14:52:45 +0800

    提交

--- src/main/java/com/clever/git/Apple.java
+++ src/main/java/com/clever/git/Apple.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("hey apple");
 	System.out.println("hello apple");
 	System.out.println("hot fix");
+	System.out.println("hot fix");
 }
 }
